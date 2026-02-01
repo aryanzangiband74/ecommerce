5a7b0dcaf63aa0f5dc55831b2b03f457d70cc9f0
Aryan <a.zangiband74@gmail.com>
2026-02-01 21:04:23 +0330

    gfhg

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { In, Repository } from 'typeorm';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>
+  ) {}
+  async create(createProductDto: CreateProductDto) {
+    const { categoryIds, ...formData } = createProductDto;
+    const product = await this.productRepository.create({ ...formData });
+
+    if (!!categoryIds && categoryIds?.length > 0) {
+      const categories = await this.categoryRepository.findBy({ id: In(categoryIds) });
+      product.categories = categories;
+    }
+    return await this.productRepository.save(product);
   }
 
   findAll() {
