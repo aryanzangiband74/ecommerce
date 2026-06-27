da01a764b41fda8e47d98e3245b2e6159a574fb9
Aryan <a.zangiband74@gmail.com>
2026-06-28 00:59:10 +0330

    Refactor docker-compose configuration: rename file from docker-compose.yaml to docker-compose.yml, add health checks for Postgres and Redis services, and remove unnecessary default environment variable values.

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,11 @@ import { IpTrackerModule } from './ipTracker/ipTracker.module'
 import { OrdersModule } from './orders/orders.module'
 import { ProductsModule } from './products/products.module'
 import { SeederModule } from './seeder/seeder.module'
+import { SmsModule } from './sms/sms.module'
 import { TasksModule } from './tasks/tasks.module'
 import { TicketsModule } from './tickets/tickets.module'
 import { PermmisionsGuard } from './users/guards/permissions.guard'
 import { UsersModule } from './users/users.module'
-import { SmsModule } from './sms/sms.module';
 
 @Module({
   imports: [
